8f5163b4f88f4ce548cfe11ecf5c08617c9609f8
Christian Lechner <lechnerc77@users.noreply.github.com>
2021-10-22 08:13:57 +0200

    sample durable function

---
+++ DurableFunctionsOrchestrator/index.ts
@@ -0,0 +1,14 @@
+﻿import * as df from "durable-functions"
+
+const orchestrator = df.orchestrator(function* (context) {
+
+    const outputs = []
+
+    outputs.push(yield context.df.callActivity("HelloDevAdvocate", "Josh"))
+    outputs.push(yield context.df.callActivity("HelloDevAdvocate", "DJ"))
+    outputs.push(yield context.df.callActivity("HelloDevAdvocate", "Marius"))
+
+    return outputs
+})
+
+export default orchestrator
---
+++ HelloDevAdvocate/index.ts
@@ -0,0 +1,9 @@
+﻿import { AzureFunction, Context } from "@azure/functions"
+
+const activityFunction: AzureFunction = async function (context: Context): Promise<string> {
+ 
+    return `Hello Developer Advocate ${context.bindings.name}!`
+
+}
+
+export default activityFunction
