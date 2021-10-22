1ca636adcdf05a0fb2f5798b11bed1e5872beaf8
Christian Lechner <lechnerc77@users.noreply.github.com>
2021-10-22 08:00:02 +0200

    sample input binding

--- HttpTriggerDevtoberfest/index.ts
+++ HttpTriggerDevtoberfest/index.ts
@@ -1,16 +1,30 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    
-    context.log('HTTP trigger function processed a request.')
-    
-    const name = req.query.name
-    
-    const responseMessage = name
-        ? "Hello Devtoberfest participant, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+
+    let responseStatusCode: number
+    let responseMessage: string
+    let responseHeaders: object
+
+
+    if (context.bindings.config) {
+
+        responseStatusCode = 200
+        responseHeaders = { "Content-Type": "application/json" }
+        responseMessage = context.bindings.config
+
+    }
+    else {
+
+        responseStatusCode = 404
+        responseHeaders = { "Content-Type": "text/plain" }
+        responseMessage = `No result found`
+
+    }
 
     context.res = {
+        status: responseStatusCode,
+        headers: responseHeaders,
         body: responseMessage
     }
 
